bc307888d8d92921b74b20a8762889d683a3bb91
manzil-infinity180 <rahulvs2809@gmail.com>
2024-07-28 17:42:55 +0530

    added message schema

--- backend/model/chatModel.js
+++ backend/model/chatModel.js
@@ -1,18 +1,5 @@
 import mongoose from "mongoose";
 
-const messageSchema= new mongoose.Schema({
-    sender:{
-        type:String,
-        required:true
-    },
-    receiver:{
-        type:String,
-        required:true
-    },
-    message:{type:String, required:true},
-    timestamp:{type:Date,default:Date.now},
-});
-
 const chatSchema= new mongoose.Schema({
     sender:{
         type:String,
