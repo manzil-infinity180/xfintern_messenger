96d60f7e93f2c0b5f6013c6904b2586ab64808ef
manzil-infinity180 <rahulvs2809@gmail.com>
2024-08-18 23:32:28 +0530

    socket connection improvement

--- chatBot_frontend/src/socket.js
+++ chatBot_frontend/src/socket.js
@@ -2,11 +2,27 @@ import { io } from 'socket.io-client';
 // setting up url
 const URL = 'http://localhost:3000';
 // getting the sever using the io fro socket.io
-export const socket = io(URL,{
-    transports: ['websocket', 'polling', 'flashsocket'],
-    withCredentials: true,
-    autoConnect:'false'
-});
+export let socket;
+export function socketConnection(){
+    if(socket && socket.connected) {
+        console.log("you are already connected");
+        return socket;
+    }else {
+         socket = io(URL,{
+                transports: ['websocket', 'polling', 'flashsocket'],
+                withCredentials: true,
+                autoConnect:'false'
+        });
+        return socket;
+    }
+}
+socket = socketConnection();
+
+// export const socket = io(URL,{
+//     transports: ['websocket', 'polling', 'flashsocket'],
+//     withCredentials: true,
+//     autoConnect:'false'
+// });
 
 // By default, the Socket.IO client opens a connection to the server right away.
 // You can prevent this behavior with the autoConnect option:
